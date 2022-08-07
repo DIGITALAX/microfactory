a99ca0a8859bbfc15c92ef5ca8d60a2221746f71
digitalax_ <72898465+digitalax1@users.noreply.github.com>
2022-08-07 00:41:38 -0400

    button responsive

--- src/components/MintSection/index.js
+++ src/components/MintSection/index.js
@@ -1,18 +1,17 @@
 import React, { useState } from "react";
 import YellowButton from "@components/YellowButton";
 import styles from "./styles.module.scss";
+import { stubTrue } from "lodash";
 
-const guideImages = [
-  '/images/homepage/pfp/panel1.png',
-]
+const MintSection = () => {
 
-const MintSection = (props) => {
+  const [styleState, setStyleState] = useState(false);
 
-  const [clicked, setClicked] = useState(styles.style1);
-
-  const clickMe = () => {
-    setClicked(styles.clickedStyle);
+  const setStyleClicked = () => {
+    styleState ? setStyleState(false): setStyleState(true);
   }
+
+  console.log(styleState);
 
   return (
     <div className={styles.PFPSectionWrapper}>
@@ -22,12 +21,12 @@ const MintSection = (props) => {
       <div className={styles.title}>
       Each instructable collected, shared and recycled puts another pixel <br></br> brick on the build map for a hyper-local web3 microfactory, virtually, <br></br>and possibly IRL, near you.
       </div>
-      <YellowButton className={styles.pfpButton} onClick={clickMe}>
+      <button className={styles.pfpButton} onClick={setStyleClicked}>
       Mint Live Soon
-      </YellowButton>
-      {/* <div className={styles.clickedStyle}>
-      Read <a href="https://www.port15.digifizzy.xyz/" target="_blank" rel="noreferrer"  style={{ color: "white" }} >DIGIFIZZY</a> while you wait.
-      </div> */}
+      </button>
+      <p className={styleState ? styles.styleClicked : styles.style1}>
+        <a href="https://port15.digifizzy.xyz/magazines/15/" target="_blank" rel="noreferrer"  className={styles.linked}>Read DIGIFIZZY while you wait.</a>
+      </p>
       
     </div>
   );
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,19 +4,6 @@ import Document, { NextScript, Html, Main, Head } from "next/document";
 
 
 class MyDocument extends Document {
-  getPreloadFontsLinks() {
-    const fontSizes = [400, 600, 700, 800, 900];
-    return fontSizes.map((size) => (
-      <link
-        rel="preload"
-        key={size}
-        as="font"
-        href={`/fonts/inter-${size}.woff2`}
-        type="font/woff2"
-        crossOrigin=""
-      />
-    ));
-  }
 
   render() {
     return (
@@ -66,57 +53,10 @@ class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Atkinson+Hyperlegible:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900"
           ></link>
-          {this.getPreloadFontsLinks()}
           {/* ipfs next.js fix */}
           <style
             dangerouslySetInnerHTML={{
               __html: `
-            @font-face {
-              font-family: "inter";
-              font-display: swap;
-              src: url("./fonts/inter-400.woff2") format("woff2"),
-              url("./fonts/inter-400.woff") format("woff");
-              font-weight: normal;
-              font-style: normal;
-            } 
-          
-            @font-face {
-              font-family: "inter";
-              font-display: swap;
-              src: url("./fonts/inter-600.woff2") format("woff2"),
-              url("./fonts/inter-600.woff") format("woff");
-              font-weight: 600;
-              font-style: normal;
-            }
-            
-            @font-face {
-              font-family: "inter";
-              font-display: swap;
-              src: url("./fonts/inter-700.woff2") format("woff2"),
-                url("./fonts/inter-700.woff") format("woff");
-              font-weight: 700;
-              font-style: normal;
-            }
-          
-            @font-face {
-              font-family: "inter";
-              font-display: swap;
-              src: url("./fonts/inter-800.woff2") format("woff2"),
-                url("./fonts/inter-800.woff") format("woff");
-              font-weight: 800;
-              font-style: normal;
-            }
-
-      
-            
-            @font-face {
-              font-family: "inter";
-              font-display: swap;
-              src: url("./fonts/inter-900.woff2") format("woff2"),
-                url("./fonts/inter-900.woff") format("woff");
-              font-weight: 900;
-              font-style: normal;
-            }
 
             @font-face {
               font-family: "SewnTight";
